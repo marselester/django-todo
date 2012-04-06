7a5ceb38c8f99de520acfc7f4141b6d8188d3625
marselester <marselester@ya.ru>
2012-04-06 14:27:04 +0600

    Добавлен тест статуса WORK по статусу предыдущей задачи

--- todo/tests.py
+++ todo/tests.py
@@ -75,5 +75,26 @@ class ActualStatusTest(TestCase):
     def testPrevTaskDone(self):
         """Тестирует статус WORK, проверяя статус предыдущей задачи.
 
-        Статус предыдущей задачи должен быть DONE.
+        Статус предыдущей задачи должен быть DONE. Цепочка начала работать 2
+        дня назад, на дизайн выделено 3 дня, задача была выполнена за 2 дня.
+        Программист досрочно (на 1 день раньше) получил результаты работы
+        дизайнера.
         """
+        now = datetime.now()
+        chain_start_date = now - timedelta(days=2)
+        chain = Chain.objects.create(name='Chain', start_date=chain_start_date,
+                                     owner=self.manager)
+        deadline_first_task = chain_start_date + timedelta(days=3)
+        first_task = Task.objects.create(worker=self.designer, task='Design',
+                                         deadline=deadline_first_task,
+                                         finish_date=now,
+                                         chain=chain,
+                                         order=Task.FIRST_TASK,
+                                         status=Task.DONE_STATUS)
+        deadline_second_task = deadline_first_task + timedelta(days=2)
+        second_task = Task.objects.create(worker=self.programmer,
+                                          task='Programming',
+                                          deadline=deadline_second_task,
+                                          chain=chain,
+                                          order=Task.FIRST_TASK + 1)
+        self.assertEqual(second_task.actual_status(), Task.WORK_STATUS)
