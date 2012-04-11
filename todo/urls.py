def547a2bdd7e0287050681e3b60ecdbd468bde4
marselester <marselester@ya.ru>
2012-04-11 13:40:31 +0600

    Добавлен url для страницы task_archive

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('todo.views',
     url(r'^$', 'actual_tasks', name='todo_actual_tasks'),
+    url(r'^task/archive/$', 'task_archive', name='todo_task_archive'),
     url(r'^task/(?P<task_id>\d+)/$', 'task_detail', name='todo_task_detail'),
 )
