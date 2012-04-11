5fe009af2453172b46a676fd27302fdc2f9abbd4
marselester <marselester@ya.ru>
2012-04-11 13:42:01 +0600

    Добавлен url для страницы chain_archive

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,6 @@ urlpatterns = patterns('todo.views',
     url(r'^$', 'actual_tasks', name='todo_actual_tasks'),
     url(r'^task/archive/$', 'task_archive', name='todo_task_archive'),
     url(r'^task/(?P<task_id>\d+)/$', 'task_detail', name='todo_task_detail'),
+
+    url(r'^chain/archive/$', 'chain_archive', name='todo_chain_archive'),
 )
